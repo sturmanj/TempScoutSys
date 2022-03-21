cefaf1f5a9521de1bed670dfb853932b4471d8c5
sturmanj <sturmanj@catlin.edu>
2022-03-20 17:40:34 -0700

    ✨  Added match number to teams map with TBA API

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,23 @@
 require("dotenv").config()
+const fetch = require("node-fetch")
 var express = require("express")
-const http = require('http')
-const socketio = require('socket.io')
-const jp = require('jsonpath')
+const http = require("http")
+const io = require("socket.io")(http)
+const jp = require("JSONpath")
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const twilioClient = require("twilio")(accountSid, authToken)
 const { App } = require("@slack/bolt")
 const { google } = require("googleapis")
 
-let client, googleSheets, spreadsheetId
+let client, googleSheets, spreadsheetId, matchToTeams
 
-//Initialize Slack, Google Sheets, and Twilio
-
+//Initialize SocketIO, Slack, Google Sheets, and Twilio
 const server = http.createServer(express())
 
 server.listen(process.env.IO_PORT, () => {
-  console.log(`SocketIO Server started on port ${process.env.IO_PORT}!`)
+	console.log(`SocketIO Server started on port ${process.env.IO_PORT}`)
 })
-
 
 const slackApp = new App({
 	token: process.env.SLACK_BOT_TOKEN,
@@ -28,19 +27,66 @@ const slackApp = new App({
 const auth = new google.auth.GoogleAuth({
 	keyFile: "APIkeys.json",
 	scopes: "https://www.googleapis.com/auth/spreadsheets",
-});
+})
 
-//Async Initializations
-(async () => {
+//Async Initialization
+;(async () => {
 	await slackApp.start(process.env.APP_PORT)
 	console.log(`Slack App started on port ${process.env.APP_PORT}`)
 	client = await auth.getClient()
 	googleSheets = google.sheets({ version: "v4", auth: client })
 	spreadsheetId = process.env.SPREADSHEET_ID
-	list = await getTeamPriority()
-	console.log(list);
+	// list = await getTeamPriority()
+	// console.log(list)
 })()
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// io.on("connection", (socket) => {
+// 	io.emit("refresh", data)
+// 	socket.on("join", (obj) => {
+// 		io.emit("refresh", data)
+// 	})
+// 	socket.on("leave", (obj) => {
+// 		io.emit("refresh", data)
+// 	})
+// })
+
+fetch("https://www.thebluealliance.com/api/v3/event/2022miliv/matches/simple", {
+	method: "GET",
+	headers: {
+		"X-TBA-Auth-Key":
+			"z6Y2VRevkgac8q9f9tReGE8K1iPvhSM4zJzRMfikF3WHyQhfzdr6uizUMS6QPWWA",
+	},
+})
+	.then((response) => response.json())
+	.then((data) => {
+		matchNums = jp.query(data, "$.*.match_number")
+		teams = jp.query(data, "$.*.*.*.team_keys")
+
+		matchToTeams = new Map()
+		for (let i = 0; i < matchNums.length; i++) {
+			matchToTeams.set(
+				matchNums[i],
+				teams[2 * i].concat(teams[2 * i + 1])
+			)
+		}
+	})
+	.catch((err) => {
+		console.error(err)
+	})
+
+//for testing:
+// const data = require("./testRes.json")
+// matchNums = jp.query(data, "$.*.match_number")
+// teams = jp.query(data, "$.*.*.*.team_keys")
+// matchToTeams = new Map()
+// for (let i = 0; i < matchNums.length; i++) {
+// 	matchToTeams.set(matchNums[i], teams[2 * i].concat(teams[2 * i + 1]))
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Helpful functions
 async function getTeamPriority() {
 	const data = await googleSheets.spreadsheets.values.batchGet({
 		auth: auth,
@@ -49,7 +95,7 @@ async function getTeamPriority() {
 		ranges: process.env.SHEET_SCOUT_RANGE,
 	})
 
-    return data.data.valueRanges[0].values[0]
+	return data.data.valueRanges[0].values[0]
 }
 
 async function textMessage(number, message) {
