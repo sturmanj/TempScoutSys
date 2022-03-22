578edb82c35efd109e6726cf15be6cf1b6d52181
sturmanj <sturmanj@catlin.edu>
2022-03-22 12:58:21 -0700

    ✨ Now works as a pwa. Offline mode is broken

--- frontend/app.js
+++ frontend/app.js
@@ -4,3 +4,19 @@ if ("serviceWorker" in navigator) {
 		.then((reg) => console.log("service worker registered", reg))
 		.catch((err) => console.log("service worker not registered", err))
 }
+
+var button = document.getElementById("clock-in")
+
+button.onclick = () => {
+	if (button.classList.contains("join")) {
+		button.classList.remove("join")
+		button.classList.add("leave")
+		button.innerText = "Leave Queue"
+	} else {
+		if (confirm("Are you sure you want to leave?")) {
+			button.classList.remove("leave")
+			button.classList.add("join")
+			button.innerText = "Join Queue"
+		}
+	}
+}
