f24759103ec7c9da5dfb32a7023d66408988a0d7
sturmanj <sturmanj@catlin.edu>
2022-03-28 15:34:46 -0700

    ✨ Added scout assignment logic and error alerting.

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,11 @@ const auth = new google.auth.GoogleAuth({
 	client = await auth.getClient()
 	googleSheets = google.sheets({ version: "v4", auth: client })
 	spreadsheetId = process.env.SPREADSHEET_ID
-	// list = await getTeamPriority()
-	// console.log(list)
 })()
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+var timeToMatch
+
 fetch("https://www.thebluealliance.com/api/v3/event/2022wimi/matches/simple", {
 	method: "GET",
 	headers: {
@@ -71,7 +71,8 @@ fetch("https://www.thebluealliance.com/api/v3/event/2022wimi/matches/simple", {
 		}
 	})
 	.catch((err) => {
-		console.error(err)
+		console.log(`Failed to get match listings from TBA`)
+		slackMessage("U01AEHK9K6Y", "Failed to get match listings from TBA")
 	})
 
 app.post("/join", (req, res) => {
@@ -97,6 +98,10 @@ app.post("/join", (req, res) => {
 			})
 		} catch (err) {
 			console.log(`Failed to add scout ${req.body.id} to the active list`)
+			slackMessage(
+				"U01AEHK9K6Y",
+				`Failed to add scout ${req.body.id} to the active list`
+			)
 			res.json({ success: false })
 			return
 		}
@@ -115,7 +120,7 @@ app.post("/leave", (req, res) => {
 				ranges: process.env.SHEET_SCOUT_RANGE,
 			})
 			ids = JSON.parse(data.data.valueRanges[0].values[0]).activeIds
-			if (!(ids.includes(req.body.id))) {
+			if (!ids.includes(req.body.id)) {
 				res.json({ success: false })
 				return
 			}
@@ -131,6 +136,10 @@ app.post("/leave", (req, res) => {
 			})
 		} catch (err) {
 			console.log(
+				`Failed to remove scout ${req.body.id} from active list`
+			)
+			slackMessage(
+				"U01AEHK9K6Y",
 				`Failed to remove scout ${req.body.id} from active list`
 			)
 			res.json({ success: false })
@@ -155,6 +164,10 @@ app.post("/verify", (req, res) => {
 			).validIds.includes(req.body.id)
 		} catch (err) {
 			console.log(`Failed to get valid scout Id ${req.body.id}`)
+			slackMessage(
+				"U01AEHK9K6Y",
+				`Failed to get valid scout Id ${req.body.id}`
+			)
 			res.json({ success: false })
 			return
 		}
@@ -169,19 +182,79 @@ app.post("/verify", (req, res) => {
 // times = jp.query(data, "$.*.predicted_time")
 // times = times.map((time) => roundTime(time * 1000))
 // teams = jp.query(data, "$.*.*.*.team_keys")
+// teams.forEach(function (teamGroup) {
+// 	teamGroup.forEach(function (team, index) {
+// 		teamGroup[index] = teamGroup[index].split("frc")[1]
+// 	})
+// })
+// matchNums = jp.query(data, "$.*.match_number")
 // timeToTeams = new Map()
 // for (let i = 0; i < times.length; i++) {
 // 	timeToTeams.set(times[i], teams[2 * i].concat(teams[2 * i + 1]))
+// }
+// timeToMatch = new Map()
+// for (let i = 0; i < matchNums.length; i++) {
+// 	timeToMatch.set(times[i], matchNums[i])
 // }
 
 //Check if the current time is time to alert for a match.
 setInterval(function () {
 	currentTime = roundTime(new Date().getTime()) + 120
 	if (timeToTeams.has(currentTime)) {
-		console.log(
-			timeToTeams.get(currentTime) + "   " + timeToMatch.get(currentTime)
-		)
-		timeToTeams.delete(currentTime)
+		;(async () => {
+			await new Promise((resolve) => setTimeout(resolve, 5000))
+			try {
+				const matchNum = timeToMatch.get(currentTime)
+				const matchTeams = timeToTeams.get(currentTime)
+				let data = await googleSheets.spreadsheets.values.batchGet({
+					auth: auth,
+					spreadsheetId: spreadsheetId,
+					majorDimension: "COLUMNS",
+					ranges: process.env.SHEET_PRIORITY_RANGE,
+				})
+
+				let teamPriorityList = []
+
+				data.data.valueRanges[0].values[0].forEach(function (entry) {
+					entry = entry.split("  ")
+					teamPriorityList.push(entry[0])
+				})
+
+				data = await googleSheets.spreadsheets.values.batchGet({
+					auth: auth,
+					spreadsheetId: spreadsheetId,
+					majorDimension: "COLUMNS",
+					ranges: process.env.SHEET_SCOUT_RANGE,
+				})
+				const activeScouts = JSON.parse(
+					data.data.valueRanges[0].values[0]
+				).activeIds
+
+				teamPriorityList.forEach((team) => {
+					if (matchTeams.includes(team)) {
+						if (activeScouts.length > 0) {
+							// twilioClient.messages.create({
+							// 	to: "+1" + activeScouts[0],
+							// 	from: process.env.TWILIO_NUMBER,
+							// 	body: `For match ${matchNum}, you will be scouting team ${team}`,
+							// })
+							console.log(
+								activeScouts[0],
+								`For match ${matchNum}, you will be scouting team ${team}`
+							)
+							activeScouts.shift()
+						}
+					}
+				})
+				timeToTeams.delete(currentTime)
+			} catch (err) {
+				console.log(`Failed to send match notifications`)
+				slackMessage(
+					"U01AEHK9K6Y",
+					"Failed to send match notifications"
+				)
+			}
+		})()
 	}
 }, 60000)
 
@@ -190,29 +263,6 @@ setInterval(function () {
 //Helpful functions
 function roundTime(timestamp) {
 	return Math.floor(new Date(timestamp / 60000) * 60000) / 1000
-}
-
-async function getTeamPriority() {
-	let data
-	try {
-		data = await googleSheets.spreadsheets.values.batchGet({
-			auth: auth,
-			spreadsheetId: spreadsheetId,
-			majorDimension: "COLUMNS",
-			ranges: process.env.SHEET_PRIORITY_RANGE,
-		})
-	} catch (err) {
-		console.log("Failed to get Team Priority List")
-	}
-
-	teamPriorityList = []
-
-	data.data.valueRanges[0].values[0].forEach(function (entry) {
-		entry = entry.split("  ")
-		teamPriorityList.push(entry[0])
-	})
-
-	return teamPriorityList
 }
 
 async function textMessage(number, message) {
